653a0ded358ef5f01f905a68054e64defc260e86
eastwood <miaorenjun0325@yahoo.co.jp>
2017-03-07 17:09:57 +0900

    addfile

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+var server = app.listen(8000,'localhost',function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
